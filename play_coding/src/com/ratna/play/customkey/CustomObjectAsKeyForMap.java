274ebaa5e04d2a1389593fc5dd6d6a46d3b04ee6
Ratna1992 <ratnasrinivas1992@gmail.com>
2022-10-05 16:27:45 +0530

    map-user-defined-object-key

---
+++ play_coding/src/com/ratna/play/customkey/CustomObjectAsKeyForMap.java
@@ -0,0 +1,29 @@
+package com.ratna.play.customkey;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ratna.play.model.Book;
+
+public class CustomObjectAsKeyForMap {
+
+	public static void main(String[] args) {
+		Map<Book, String> bookMap = new HashMap<>();
+
+		bookMap.put(new Book("Java", "James Gosling"), "Java");
+		bookMap.put(new Book("C++", "Bjourn Stroustup"), "C++");
+		bookMap.put(new Book("C", "Denish Ritche"), "C");
+		bookMap.put(new Book("Databse", "C J Date"), "Database");
+
+		System.out.println();
+
+		bookMap.forEach((k, v) -> System.out.println(k + " => " + v));
+
+		System.out.println();
+
+		Book key = new Book("Java", "James Gosling");
+
+		System.out.println("Is key: " + key + " available? " + bookMap.containsKey(key));
+	}
+
+}
