eb39d815674163561beca00f51517e2b767b5976
Ratna1992 <ratnasrinivas1992@gmail.com>
2022-10-30 16:11:51 +0530

    threads

---
+++ play_coding/src/com/ratna/play/coding/threads/Counter.java
@@ -0,0 +1,14 @@
+package com.ratna.play.coding.threads;
+
+public class Counter {
+	int count = 0;
+
+	public void increment() {
+		count = count + 1;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+}
