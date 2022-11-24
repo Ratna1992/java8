8919c40d67771a749bf4b40d387294d21be108b1
Ratna1992 <ratnasrinivas1992@gmail.com>
2022-11-24 21:53:00 +0530

    serialization/deserialization

---
+++ play_coding/src/com/ratna/play/coding/deserialization/DeserializationExample.java
@@ -0,0 +1,31 @@
+package com.ratna.play.coding.deserialization;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import com.ratna.play.coding.serialization.Student;
+
+public class DeserializationExample {
+
+	public static void main(String[] args) {
+		try {
+
+			// Creating FileInputStream object.
+			FileInputStream fos = new FileInputStream("student.json");
+
+			// Creating an ObjectInputStream object.
+			ObjectInputStream oos = new ObjectInputStream(fos);
+
+			// read object.
+			Student student = (Student) oos.readObject();
+			System.out.println(student.getName() + " " + student.getRollNo());
+			// close streams.
+			oos.close();
+			fos.close();
+		} catch (IOException | ClassNotFoundException e) {
+			System.out.println(e);
+		}
+	}
+
+}
